da674f80ccba3c77cc4524cbb02bb12e95e6dbee
Sotiris Fanou <sotiris.fanou@gmail.com>
2022-03-22 16:06:33 +0200

    defines separate functions to be used as callbacks

--- sotirisCallback1.js
+++ sotirisCallback1.js
@@ -3,21 +3,27 @@ wants the function to be called at a specific time */
 
 function checkString(str, callback) {
   if (!str || str === "") {
-    callback("The string is empty");
+    callback("The string is empty.");
     return;
   }
   console.log(str + "additional text");
 }
 
-/* here the call back function is defined in the parameter list 
-because I do not want to change the functionality of the callback */
-checkString("", function myCallback(msg) {
-  console.log(msg);
-  return;
-});
+/* here a callback function is defined as a separate function
+ and it is then passed as parameter when checkString() is called */
 
-/* notice that I am using the same function checkString(). When 
-writing its definition I set where/when the callback function will 
-be called. Then in the parameter list I defined what the callback 
-function will be doing
-*/
+function myCallback1(msg) {
+  console.log(msg + " This message was in myCallback1");
+  return;
+}
+
+/* definition of another function which can be passed as a callback */
+function myCallback2(msg) {
+  console.log(msg + " This message was in myCallback2");
+  return;
+}
+
+// CALLING checkString()
+checkString("", myCallback1);
+/* I can pass a different callback to checkString() */
+checkString("", myCallback2);
