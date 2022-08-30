07b488b82434c718036f29524ffffbc2db34fd1a
Sotiris Fanou <sotiris@investotech.com>
2022-08-30 09:36:40 +0300

    datetime tests

---
+++ datetime1.js
@@ -0,0 +1,10 @@
+let dt = new Date("2022-08-29 08:00:00");
+
+//console.log("the date is ", dt);
+
+let dateNow = new Date();
+
+console.log("the date now is ", dateNow);
+console.log("the date now in UTC is ", dateNow.toUTCString());
+console.log("the date toDateString ", dateNow.toDateString());
+console.log("the date toISOString ", dateNow.toISOString());
